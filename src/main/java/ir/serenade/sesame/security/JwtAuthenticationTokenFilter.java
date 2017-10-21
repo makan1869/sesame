ad135b5b7f7369e48812fb90847273a827de73aa
Makan Sepehri <makan1869@gmail.com>
2017-10-21 23:30:14 +0330

    Fixing Import Problem

--- src/main/java/ir/serenade/sesame/security/JwtAuthenticationTokenFilter.java
+++ src/main/java/ir/serenade/sesame/security/JwtAuthenticationTokenFilter.java
@@ -1,5 +1,7 @@
 package ir.serenade.sesame.security;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import ir.serenade.sesame.utils.JwtTokenUtil;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
