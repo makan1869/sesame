7a27556ed737fa4c45d054686ae905cfd38eab9c
Makan Sepehri <makan1869@gmail.com>
2017-10-23 09:44:40 +0330

    Adding Device

---
+++ src/main/java/ir/serenade/sesame/domain/Device.java
@@ -0,0 +1,72 @@
+package ir.serenade.sesame.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Device {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+
+    @ManyToOne
+    User user;
+
+    String uuid;
+
+    String deviceName;
+
+    String deviceType;
+
+    String deviceOS;
+
+    public Device() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getDeviceName() {
+        return deviceName;
+    }
+
+    public void setDeviceName(String deviceName) {
+        this.deviceName = deviceName;
+    }
+
+    public String getDeviceType() {
+        return deviceType;
+    }
+
+    public void setDeviceType(String deviceType) {
+        this.deviceType = deviceType;
+    }
+
+    public String getDeviceOS() {
+        return deviceOS;
+    }
+
+    public void setDeviceOS(String deviceOS) {
+        this.deviceOS = deviceOS;
+    }
+}
